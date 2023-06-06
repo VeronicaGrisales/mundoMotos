1834ddc6ff6c1fcce55005aac6de6e271df77999
VeronicaGrisales <verogrisales4@gmail.com>
2023-06-05 21:34:01 -0500

    Renombrando las carpetas

--- motos/src/Componentes/segundoComponente/registrarse.js
+++ motos/src/Componentes/segundoComponente/registrarse.js
@@ -3,8 +3,8 @@ import ReactDOM from'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-function Ventas(){
-    return <h1>Aca podras hacer varias compras de nuestros productos</h1>;
+function Registrarse(){
+    return <h1>Mi primer componente</h1>;
 }
 
-export default Ventas;
+export default Registrarse;
--- motos/src/index.js
+++ motos/src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from'react-dom/client';
-import Ventas from './Componentes/primerComponente/primerComponente';
-import Componente from './Componentes/segundoComponente/segundoComponente';
+import Ventas from './Componentes/primerComponente/ventas';
+import Registrarse from './Componentes/segundoComponente/registrarse';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-
-root.render (
+root.render(
     <>
-    <Componente/>
+    <Registrarse/>
     <Ventas/>
     </>
     );
-
