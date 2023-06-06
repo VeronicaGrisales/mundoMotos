73906f589a94d5009ef463776a63f6522eefd65c
carlosRivas <galanh1.1@gmail.com>
2023-06-06 00:53:12 -0500

    primeros pasos

---
+++ motos/src/Componentes/heade/index.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render(){
+        return(
+            <div className="header" id='inicio'>
+                <div className="buscador">
+                    <input type='tex' placeholder='Buscar'/>
+                    <button type='submit'>
+                        <h1>Aceptar</h1>
+                    </button>
+                    <div className='btn'>
+                        <i className='bx bx-search'/>
+                    </div>
+                </div>
+                <div className='carrito'>
+                    <i className='bx bx-cart'/>
+                </div>
+                <img src='#' alt='' className='hamburger'/>
+                <div className='menu_principal'>
+                    <nav lassName='menu_registro'>
+                        <a href='#'>Inicio</a> <br/>
+                        <a href='#'>Servicios</a> <br/>
+                        <a href='#'>¿Que ofrecemos?</a> <br/>
+                        <a href='#'>Galeria</a> <br/>
+                        <a href='#'>Expertos en</a> <br/>
+                        <a href='#'>Contactanos</a> <br/>
+                        <a href='#'>Registrate</a> <br/>
+                        <a href='#'>Iniciar Secion</a> <br/>                        
+                    </nav>
+                </div>
+                <div className='contenedor head'>
+                    <div className='inicio' id='menu'>
+                        <ul>
+                            <li id='logo'>
+                                <a href='#' >
+                                    <img src='#' width={150} alt='' />
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <h1 className='titulo'>Visítanos y convierte tus sueños en realidad</h1>
+                    <p className='copy'>Tenemos los mejores productos del mercado y a mejor precio.</p>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Header
--- motos/src/index.js
+++ motos/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from'react-dom/client';
 import Ventas from './Componentes/primerComponente/primerComponente';
 import Componente from './Componentes/segundoComponente/segundoComponente';
+import Header from './Componentes/heade';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -10,6 +11,7 @@ root.render (
     <>
     <Componente/>
     <Ventas/>
+    <Header/>
     </>
     );
 
