5a17b69f015e5b9a5901ef21bc5aa2748f451b30
VeronicaGrisales <verogrisales4@gmail.com>
2023-06-05 22:36:11 -0500

    Formulario de registro

--- motos/src/Componentes/segundoComponente/registrarse.js
+++ motos/src/Componentes/segundoComponente/registrarse.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import ReactDOM from'react-dom/client';
+import './registro.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-function Registrarse(){
-    return <h1>Mi primer componente</h1>;
-}
+function Registrarse () {
+    
+    return (
+    <section className='form-register'>
+
+        <h4>Formulario de registro</h4>
+
+        <input className='controls' type='texto' name='nombres' id='nombres' placeholder='Ingrese su Nombre'></input>
+        <input className='controls' type='texto' name='apellidos' id='apellidos' placeholder='Ingrese su Apellido'></input>
+        <input className='controls' type='email' name='correo' id='correo' placeholder='Ingrese su Correo electronico'></input>
+        <input className='controls' type='password' name='contraseña' id='contraseña' placeholder='Ingrsese su Contrseña'></input>
+        
+        <p>
+            Estoy de acuerdo con 
+            <a href='#'> terminos y condiciones</a>
+        </p>
+
+        <button className='boton'>Registrarse</button>
+        <p>
+        <a href='#'>
+        ¿Ya tengo cuenta?
+        </a>
+        </p>
+    </section>
+    );
+};
 
 export default Registrarse;
