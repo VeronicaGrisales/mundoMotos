210c82065146cc7c71e14fe4b1fdfd271bc8d55c
VeronicaGrisales <verogrisales4@gmail.com>
2023-06-07 18:39:19 -0500

    Hacemos una parte del menu

--- motos/src/index.js
+++ motos/src/index.js
@@ -5,20 +5,20 @@ import Header from './Componentes/heade';
 import Footer from './Componentes/tercerComponente/footer';
 import Registrarse from './Componentes/segundoComponente/registrarse';
 import Ventas from './Componentes/ventas/ventas';
+import Menu from './Componentes/primerComponente/menu';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render (
     <>
-    <Ventas/>
-   
+        <Menu/>
     </>
     );
 
     /*
     <Header/>
-     <Footer/>
-    
+    <Footer/>
+    <Ventas/>
     <Registrarse/>
     <Log/>
     */
