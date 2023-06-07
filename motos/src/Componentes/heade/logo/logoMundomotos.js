df2228b85f5084338b2d73f30e46c014f5ec7107
carlosRivas <galanh1.1@gmail.com>
2023-06-07 00:00:35 -0500

    stylo y formas

---
+++ motos/src/Componentes/heade/logo/logoMundomotos.js
@@ -0,0 +1,21 @@
+//import React from "react";
+import Logo from '../../../Componentes/la_pagina_web/logo.png';
+import './logoMundomotos.css';
+
+
+function Log() {
+
+  return (
+
+    <div className='inicio' id='menu'>
+      <ul>
+        <li id='logo'>
+          <a href='#' >
+            <img src={Logo} width={150} alt='' />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+export default Log;
--- motos/src/index.js
+++ motos/src/index.js
@@ -1,33 +1,21 @@
 import React from 'react';
 import ReactDOM from'react-dom/client';
-<<<<<<< HEAD
 import Ventas from './Componentes/primerComponente/primerComponente';
 import Componente from './Componentes/segundoComponente/segundoComponente';
+import Log from './Componentes/heade/logo/logoMundomotos';
 import Header from './Componentes/heade';
-=======
-import Ventas from './Componentes/primerComponente/ventas';
-import Footer from './Componentes/tercerComponente/footer';
-/*import Registrarse from './Componentes/segundoComponente/registrarse';*/
->>>>>>> ec01fc21329af2fc8b95c57848beabcee19a589b
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
 root.render (
     <>
-<<<<<<< HEAD
-    <Componente/>
-    <Ventas/>
     <Header/>
-    </>
-    );
-
-=======
+    {/*<Componente/>
     <Ventas/>
-   <Footer/>
+<Log/>*/}
+   
     </>
     );
-  /*<Registrarse/>  */
 
-  
->>>>>>> ec01fc21329af2fc8b95c57848beabcee19a589b
