e0264f1e73c7c2a81a404ab305bb903b778a3132
carlosRivas <galanh1.1@gmail.com>
2023-06-07 20:55:44 -0500

    instalando dependecias del router

--- motos/src/index.js
+++ motos/src/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from'react-dom/client';
-<<<<<<< HEAD
 import Ventas from './Componentes/primerComponente/primerComponente';
 import Componente from './Componentes/segundoComponente/segundoComponente';
-=======
-import Log from './Componentes/logo/logoMundomotos';
->>>>>>> 5efbd5b0b650322b4bf9e549c916841fe535dd06
 import Header from './Componentes/heade';
 import Footer from './Componentes/tercerComponente/footer';
 import Registrarse from './Componentes/segundoComponente/registrarse';
-import Ventas from './Componentes/ventas/ventas';
+//import Ventas from './Componentes/ventas/ventas';
 import Menu from './Componentes/primerComponente/menu';
 
 
@@ -17,15 +13,15 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render (
     <>
-    <Ventas/> 
+    <Header/> 
     </>
     );
 
-    /*
+    {/*
     <Menu/> 
     <Header/>
     <Footer/>
     
     <Registrarse/>
     <Log/>
-    */
+*/}
