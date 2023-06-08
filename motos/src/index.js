c39bbf71663895532e4552851a5c1bc035745cb2
VeronicaGrisales <verogrisales4@gmail.com>
2023-06-07 21:06:58 -0500

    Solucionando problemas

--- motos/src/index.js
+++ motos/src/index.js
@@ -1,27 +1,25 @@
 import React from 'react';
 import ReactDOM from'react-dom/client';
-import Ventas from './Componentes/primerComponente/primerComponente';
-import Componente from './Componentes/segundoComponente/segundoComponente';
-import Header from './Componentes/heade';
+import Header from './Componentes/heade/index';
 import Footer from './Componentes/tercerComponente/footer';
 import Registrarse from './Componentes/segundoComponente/registrarse';
-//import Ventas from './Componentes/ventas/ventas';
+import Ventas from './Componentes/ventas/ventas';
 import Menu from './Componentes/primerComponente/menu';
-
+import Buscar from './Componentes/heade/buscador/buscador';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render (
     <>
-    <Header/> 
+    <Header/>
     </>
     );
 
     {/*
+    <Buscar/>
     <Menu/> 
-    <Header/>
-    <Footer/>
     
+    <Footer/>
     <Registrarse/>
     <Log/>
 */}
